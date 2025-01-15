f53f943878e4595422e14d75df62a234a03f0828
DESKTOP-TJ7FT2O\742221_MINI <hykqo@naver.com>
2025-01-15 20:12:15 +0900

    Eureka에 인스턴스 등록 및 자동포트 할당하여 로드밸런싱 처리. <second-service>, <first-service>

--- zuul/first-service/src/main/java/org/example/firstservice/FirsServiceController.java
+++ zuul/first-service/src/main/java/org/example/firstservice/FirsServiceController.java
@@ -1,6 +1,9 @@
 package org.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
+@RequiredArgsConstructor
 public class FirsServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -23,8 +29,12 @@ public class FirsServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request){
+        //HttpServletRequest로 서버포트 받아오기.
+        log.info("Server port={}", request.getServerPort());
+
+        //Environment로 서버포트 받아오기.
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 
 }
--- zuul/second-service/src/main/java/org/example/secondservice/SecondServiceController.java
+++ zuul/second-service/src/main/java/org/example/secondservice/SecondServiceController.java
@@ -1,6 +1,9 @@
 package org.example.secondservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/second-service")
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class SecondServiceController {
 
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -24,7 +29,11 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from Second Service";
+    public String check(HttpServletRequest request){
+        //HttpServletRequest로 서버포트 받아오기.
+        log.info("Server port={}", request.getServerPort());
+
+        //Environment로 서버포트 받아오기.
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
