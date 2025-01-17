315a8b96b8aed5a0bcf2fd6b75ca39fea0f0577d
DESKTOP-TJ7FT2O\742221_MINI <hykqo@naver.com>
2025-01-17 20:06:34 +0900

    E-commerce 애플리케이션 : user-service 프로젝트 생성 및 테스트 api생성

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableDiscoveryClient //서비스 디스커버리에 클라이언트로 등록될 애플리케이션 지정.
 public class UserServiceApplication {
 
     public static void main(String[] args) {
